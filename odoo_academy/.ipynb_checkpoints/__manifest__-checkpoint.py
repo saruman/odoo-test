28b0d6095a3fb6c19d9867fa2240a01fd1571558
saruman <evillarreal@smd.com.pa>
2021-04-06 17:25:39 +0000

    [ADD] Basic Module

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Academy',
+    'summary': "Academy app to manage training",
+    'description': """
+        Academy module to manage trainings:
+            -courses
+            -sessions
+            -attenders
+        """,
+    'author': 'Ezequiel',
+    'website': 'https://stri.si.edu',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+}
